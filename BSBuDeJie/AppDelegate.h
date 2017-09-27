e1a9e7a829cf0c13ffe9245fdd702b8a72c6ed30
Dobby <jevo747@yeah.net>
2017-09-27 10:59:52 +0800

    Initial Commit

---
+++ BSBuDeJie/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BSBuDeJie
+//
+//  Created by v_ljiayili(李嘉艺) on 2017/9/27.
+//  Copyright © 2017年 Dobby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
